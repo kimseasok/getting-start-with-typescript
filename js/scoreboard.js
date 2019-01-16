5ab4ffe5840b5802b913d1eb8db46d93cf955dfa
kimseasok <basicblogtalk@gmail.com>
2019-01-16 01:00:39 -0500

    7: install and use type decoration

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,11 +1,21 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var _ = __importStar(require("lodash"));
 var Scoreboard = (function () {
     function Scoreboard() {
         this.results = [];
     }
     Scoreboard.prototype.addResult = function (newResult) {
         this.results.push(newResult);
+        var allCapsName = _.upperCase(newResult.playerName);
+        console.log(allCapsName + ": " + newResult.score);
     };
     Scoreboard.prototype.updateScoreboard = function () {
         var output = '<h2>Scoreboard</h2>';
